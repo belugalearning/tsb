31d916586b52e4624ed463fe3bdcd163510aa6d0
Nick Cartwright <nick@aslant.io>
2013-06-05 16:18:04 +0100

    add shell bundle module

---
+++ app/scripts/services/bundle/index.js
@@ -0,0 +1,7 @@
+define(function(require) {
+  var expressionTemplate = require('text!./templates/expression.xml!strip')
+  var yTemplate = require('text!./templates/y.xml!strip')
+  var xDivYTemplate = require('text!./templates/xdivy.xml!strip')
+
+  console.log('hello')
+})
