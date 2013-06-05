920e032e22f25af2cace23f4ad7ad3c6dfa2da08
Nick Cartwright <nick@aslant.io>
2013-06-05 18:34:16 +0100

    add content service

---
+++ app/scripts/services/content-service.js
@@ -0,0 +1,24 @@
+var contentService
+
+;(function() {
+  ContentService = function() {
+    this.setBundle(null)
+  }
+
+  ContentService.prototype.setBundle = function(bundle) {
+    this.bundle = bundle
+    this.currentQuestionIx = -1
+  }
+
+  ContentService.prototype.nextQuestion = function() {
+    if (!this.bundle) return null
+    var q = this.bundle.questions[++this.currentQuestionIx]
+    return this.currentQuestion
+  }
+
+  ContentService.prototype.currentQuestion = function() {
+    return this.bundle && this.bundle.questions[this.currentQuestionIx]
+  }
+
+  contentService = new ContentService()
+}())
